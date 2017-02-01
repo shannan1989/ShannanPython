843f42e1f5686eed187581cc520f2d6d358d0897
山南 <zhangpeichaook@163.com>
2017-02-01 14:48:54 +0800

    #

--- Crawl/tumblr.py
+++ Crawl/tumblr.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+import ConfigParser
+import json
 import os
 import time
 
@@ -8,9 +10,15 @@ from lxml import etree
 
 
 class TumblrSpider(object):
-    start_urls = (
-        'http://*.tumblr.com/api/read',
-    )
+    start_urls = ()
+    crawl_all = False
+    crawl_video = False
+
+    def __init__(self):
+        config = ConfigParser.ConfigParser()
+        config.read('settings/tumblr.ini')
+        for t in config.items("tumblrs"):
+            self.start_urls += (t[1],)
 
     def parse(self):
         try:
@@ -18,7 +26,6 @@ class TumblrSpider(object):
                 "http": "http://127.0.0.1:56459",
                 "https": "http://127.0.0.1:56459",
             }
-            cookies = dict(logged_in='1')
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36'
             }
@@ -30,16 +37,20 @@ class TumblrSpider(object):
                 while start <= total:
                     url = start_url + '?num=' + str(num) + '&start=' + str(start)
                     start += num
-                    r = requests.get(url, allow_redirects=False, proxies=proxies, headers=headers, cookies=cookies)
+                    r = requests.get(url, allow_redirects=True, proxies=proxies, headers=headers)
                     print str(r.status_code) + ' ' + url
                     if r.status_code != 200:
                         continue
                     else:
                         try:
                             html = etree.HTML(r.content)
-                            title = html.xpath("//tumblelog/@title")[0]
+                            try:
+                                title = html.xpath("//tumblelog/@title")[0]
+                            except Exception as e:
+                                title = html.xpath("//tumblelog/@name")[0]
+
                             total = (int)(html.xpath("//posts/@total")[0])
-                            dir_path = '/Photo/Crawl/tumblr/' + title
+                            dir_path = '/Photo/Crawl/Tumblr/' + title
                             if not os.path.exists(dir_path):
                                 os.makedirs(dir_path)
 
@@ -54,17 +65,33 @@ class TumblrSpider(object):
 
                                 print image_url
                                 print file_path
-                                proxies = {
-                                    "http": "http://127.0.0.1:56459",
-                                    "https": "http://127.0.0.1:56459",
-                                }
                                 ir = requests.get(image_url, proxies=proxies)
                                 if ir.status_code == 200:
                                     open(file_path, 'wb').write(ir.content)
                                 pass
+
+                            if self.crawl_video is True:
+                                videos = html.xpath("//video-player")
+                                for video in videos:
+                                    data = json.loads(etree.HTML(video.text).xpath("//video/@data-crt-options")[0])
+                                    if data['hdUrl'] is False:
+                                        continue
+                                    ts = data['hdUrl'].split('/')
+                                    file_name = ts[len(ts) - 2] + '_' + ts[len(ts) - 1] + '.mp4'
+                                    file_path = '%s/%s' % (dir_path, file_name)
+                                    if os.path.exists(file_path):
+                                        continue
+                                    print data['hdUrl']
+                                    print file_path
+                                    vr = requests.get(data['hdUrl'], proxies=proxies)
+                                    if vr.status_code == 200:
+                                        open(file_path, 'wb').write(vr.content)
+
                         except Exception, e:
                             print e.message
-                    pass
+
+                    if self.crawl_all is False:
+                        break
 
         except Exception, e:
             print e.message
