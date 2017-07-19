c8530a5db71680951bd6cc5dc7cb9549371a9dc6
山南 <zhangpeichaook@163.com>
2017-07-19 21:25:16 +0800

    #

--- Crawl/TumblrSpider/spider.py
+++ Crawl/TumblrSpider/spider.py
@@ -10,6 +10,11 @@ from lxml import etree
 
 from config import *
 
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
 
 class TumblrSpider(object):
     proxies = {
@@ -36,69 +41,90 @@ class TumblrSpider(object):
                 if r.status_code != 200:
                     continue
                 else:
-                    html = etree.HTML(r.content)
+                    root = ET.fromstring(r.content)
 
-                    total = (int)(html.xpath("//posts/@total")[0])
-
+                    tumblelog = root.find('tumblelog')
                     try:
-                        title = html.xpath("//tumblelog/@title")[0]
+                        title = tumblelog.attrib.get('title')
                     except Exception as e:
-                        title = html.xpath("//tumblelog/@name")[0]
+                        title = tumblelog.attrib.get('name')
 
-                    dir_path = SavedPath + title
-                    dir_ext = TumblrPath + title
+                    posts = root.find('posts')
+                    total = (int)(posts.attrib.get('total'))
 
-                    images = html.xpath("//photo-url[@max-width='1280']")
-                    for image in images:
-                        image_url = image.text
-                        ts = image_url.split('/')
-                        file_name = ts[len(ts) - 1]
+                    for post in posts.findall('post'):
+                        date = time.strftime("%Y-%m-%d", time.localtime((float)(post.attrib.get('unix-timestamp'))))
+                        dir_path = SavedPath + title + '/' + date
+                        dir_ext = TumblrPath + title
 
-                        file_path = '%s/%s' % (dir_path, file_name)
-                        if os.path.exists(file_path):
-                            if time.time() - os.path.getctime(file_path) > 60 * 60 * 2:
-                                os.remove(file_path)
-                            else:
-                                continue
+                        for photo in post.iter('photo-url'):
+                            if photo.attrib.get('max-width') == '1280':
+                                image_url = photo.text
+                                ts = image_url.split('/')
+                                file_name = ts[len(ts) - 1]
 
-                        file_ext = '%s/%s' % (dir_ext, file_name)
-                        if os.path.exists(file_ext):
-                            continue
+                                file_path = '%s/%s' % (dir_path, file_name)
+                                if os.path.exists(file_path):
+                                    if time.time() - os.path.getctime(file_path) > 60 * 60 * 12:
+                                        os.remove(file_path)
+                                    else:
+                                        continue
 
-                        if not os.path.exists(dir_path):
-                            os.makedirs(dir_path)
+                                file_ext = '%s/%s' % (dir_ext, file_name)
+                                if os.path.exists(file_ext):
+                                    continue
 
-                        try:
-                            print "Fetching " + image_url
-                            ir = requests.get(image_url, proxies=self.proxies)
-                            if ir.status_code == 200:
-                                open(file_path, 'wb').write(ir.content)
-                                print "Saved to " + dir_path
-                        except Exception, e:
-                            print "Save Failed: " + str(e.message)
+                                if not os.path.exists(dir_path):
+                                    os.makedirs(dir_path)
 
-                    if self.setting['crawl_video'] is True:
-                        videos = html.xpath("//video-player")
-                        for video in videos:
-                            data = json.loads(etree.HTML(video.text).xpath("//video/@data-crt-options")[0])
-                            if data['hdUrl'] is False:
-                                continue
-                            ts = data['hdUrl'].split('/')
-                            file_name = ts[len(ts) - 2] + '_' + ts[len(ts) - 1] + '.mp4'
+                                try:
+                                    print "Fetching " + image_url
+                                    ir = requests.get(image_url, proxies=self.proxies)
+                                    if ir.status_code == 200:
+                                        open(file_path, 'wb').write(ir.content)
+                                        print "Saved to " + dir_path
+                                except Exception, e:
+                                    print "Save Photo Failed: " + str(e.message)
 
-                            file_path = '%s/%s' % (dir_path, file_name)
-                            if os.path.exists(file_path):
-                                continue
+                        if self.setting['crawl_video'] is True:
+                            for video in post.findall('video-player'):
+                                video_options = etree.HTML(video.text).xpath("//video/@data-crt-options")
+                                if len(video_options) <= 0:
+                                    continue
+                                data = json.loads(video_options[0])
+                                if data['hdUrl'] is False:
+                                    video_url = etree.HTML(video.text).xpath("//video//source/@src")[0]
+                                else:
+                                    video_url = data['hdUrl']
 
-                            file_ext = '%s/%s' % (dir_ext, file_name)
-                            if os.path.exists(file_ext):
-                                continue
+                                ts = video_url.split('/')
+                                if 'tumblr_' in ts[len(ts) - 1]:
+                                    file_name = ts[len(ts) - 1] + '.mp4'
+                                else:
+                                    file_name = ts[len(ts) - 2] + '_' + ts[len(ts) - 1] + '.mp4'
 
-                            print "Fetching " + data['hdUrl']
-                            vr = requests.get(data['hdUrl'], proxies=self.proxies)
-                            if vr.status_code == 200:
-                                open(file_path, 'wb').write(vr.content)
-                                print "Saved to " + file_path
+                                file_path = '%s/%s' % (dir_path, file_name)
+                                if os.path.exists(file_path):
+                                    if time.time() - os.path.getctime(file_path) > 60 * 60 * 12:
+                                        os.remove(file_path)
+                                    else:
+                                        continue
+
+                                file_ext = '%s/%s' % (dir_ext, file_name)
+                                if os.path.exists(file_ext):
+                                    continue
+
+                                if not os.path.exists(dir_path):
+                                    os.makedirs(dir_path)
+
+                                try:
+                                    print "Fetching " + video_url
+                                    vr = requests.get(video_url, proxies=self.proxies)
+                                    if vr.status_code == 200:
+                                        open(file_path, 'wb').write(vr.content)
+                                        print "Saved to " + file_path
+                                except Exception, e:
+                                    print "Save Video Failed: " + str(e.message)
 
                 if self.setting['crawl_all'] is False:
                     break
