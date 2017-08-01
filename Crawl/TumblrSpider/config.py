19889f30e79b5cc842fd5273452ad2df6c67d153
山南 <zhangpeichaook@163.com>
2017-08-01 15:18:15 +0800

    TumblrSpider - config

---
+++ Crawl/TumblrSpider/config.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+Tumblrs = [
+    {
+        'url': 'https://a.tumblr.com/api/read',
+        'crawl_all': False, 'crawl_video': True, 'page_size': 15
+    },
+    {
+        'url': 'https://b.tumblr.com/api/read',
+        'crawl_all': False, 'crawl_video': True, 'page_size': 15
+    }
+]
+
+SavedPath = '/Photo/Crawl/Tumblr/'
+
+TumblrPath = '/Photo/Tumblr/'
+
+CrawlInterval = 60 * 10
